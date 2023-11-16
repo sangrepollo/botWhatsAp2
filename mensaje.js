09b159b23f3d2dc5339c40c403c4340f8deb0335
sangrepollo <pollodesangre@gmail.com>
2023-11-16 06:47:05 -0500

    Create mensaje.js

---
+++ mensaje.js
@@ -0,0 +1,9 @@
+const mensajes = [
+    "Hola, ¿en qué puedo ayudarte?",
+    "Este es un mensaje de ejemplo.",
+    "¡Bienvenido a nuestro grupo de WhatsApp!"
+];
+
+module.exports = {
+    mensajes
+};
