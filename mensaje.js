770d41ee1b6cffce957cbd8ead5dbd8f53e84c8e
sangrepollo <pollodesangre@gmail.com>
2023-11-16 09:20:26 -0500

    Update mensaje.js

--- mensaje.js
+++ mensaje.js
@@ -1,7 +1,10 @@
 const mensajes = [
-    "Hola, ¿en qué puedo ayudarte?",
-    "Este es un mensaje de ejemplo.",
-    "¡Bienvenido a nuestro grupo de WhatsApp!"
+    "¡Ofertas imperdibles a tu alcance! 🔥 " +
+    "\n🖱️ *Mouse Gaming a solo 2,000 CUP:* Potencia tu experiencia de juego. " +
+    "\n💍 *Sortija de Oro 10k, 6.68g por 25,000 CUP:* Añade un toque de elegancia. " +
+    "\n🎧 *Equipo Recargable de Música a 2,500 CUP:* Disfruta de tus melodías favoritas. " +
+    "\n💧 *Tanque de 1,000 Litros por 40,000 CUP:* Asegura tu suministro de agua. " +
+    "\n¡No dejes pasar estas oportunidades! Contacta ahora y hazte con estos increíbles productos. 📞✨"
 ];
 
 module.exports = {
