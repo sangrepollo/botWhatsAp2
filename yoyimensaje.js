90b5bf5b374be465b0c711d6573c9e41c5554ad5
sangrepollo <pollodesangre@gmail.com>
2024-05-26 00:33:11 -0500

    Create yoyimensaje.js

---
+++ yoyimensaje.js
@@ -0,0 +1,27 @@
+const mensajes = [
+    "🎉 ¡Desafía a la suerte y gana premios increíbles con nuestra Ruleta de la Suerte! 🎲" +
+    "\n\n🔥 ¡Apuesta en nuestras filas y gana en grande! 💰" +
+    "\n💸 Apuesta $70 y gana $900" +
+    "\n💸 Apuesta $95 y gana $1300" +
+    "\n💸 Apuesta $160 y gana $2100" +
+    "\n\n🎰 ¡Juega tus números de la suerte del 1 al 20 y espera la emoción de la ruleta girando! 🔄" +
+    "\n\n📹 ¡Revelamos al ganador en un emocionante vídeo para total transparencia y confianza! 🎥" +
+    "\n\n💳 ¡Transfiere tu apuesta para jugar y captura la pantalla de tu transacción! 💳" +
+    "\n\n🤝 ¡Respeto absoluto hacia todos los participantes, cualquier falta de respeto resultará en la expulsión del grupo! 🚫" +
+    "\n\n¡Únete a la diversión y desafía a la suerte hoy mismo con nuestra Ruleta de la Suerte! 🍀",
+
+    "🎉 ¡Anuncio emocionante!" +
+    "\n\n🔥 ¡Apuesta en nuestras filas y gana en grande! 💰" +
+    "\n💸 Apuesta $70 y gana $900" +
+    "\n💸 Apuesta $95 y gana $1300" +
+    "\n💸 Apuesta $160 y gana $2100" +
+    "\n\n🎰 ¡Juega tus números de la suerte del 1 al 20 y espera la emoción de la ruleta girando! 🔄" +
+    "\n\n📹 ¡Revelamos al ganador en un emocionante vídeo para total transparencia y confianza! 🎥" +
+    "\n\n💳 ¡Transfiere tu apuesta para jugar y captura la pantalla de tu transacción! 💳" +
+    "\n\n🤝 ¡Respeto absoluto hacia todos los participantes, cualquier falta de respeto resultará en la expulsión del grupo! 🚫" +
+    "\n\n¡Únete a la diversión y desafía a la suerte hoy mismo con nuestra Ruleta de la Suerte! 🍀"
+];
+
+module.exports = {
+    mensajes
+};
